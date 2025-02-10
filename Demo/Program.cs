d6e4a5248840c8fd2c39309cf0ac0b730ffafb13
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-02-10 21:56:29 +0200

    1.filteration operation - where / oftype

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Demo
+﻿using System.Collections;
+using static Demo.ListGenerator;
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.filteration operation - where / oftype
+            // 1.filteration operation - where / oftype
+            // all product out stock
+
+            // fluent syntax
+            // var result = productlist.where(p => p.unitsinstock == 0);
+
+            // query syntax [ query expertion ]
+            //var result = from  p in productlist
+            //             where p.unitsinstock ==0
+            //             select p;
+
+            // var result = productlist.where(p => p.category == "meat/poultry");
+
+            //var result = from p in productlist
+            //             where p.category == "meat/poultry"
+            //             select p;
+
+            //var result = productlist.where(p => p.unitsinstock > 0).where(p => p.category == "meat/poultry");
+            //var result = productlist.where(p => p.unitsinstock > 0 &&  p.category == "meat/poultry");
+
+            //var result = from p in productlist
+            //             where p.unitsinstock > 0 && p.category == "meat/poultry"
+            //             select p;
+
+            // indexed where vaild in only fulent syntax
+            //var result = productlist.where((p,i) => i < 10 && p.unitsinstock > 0);
+            //var result = productlist.where((p, i) => i < 5 );
+            //var result = productlist.where(p => p.unitsinstock > 0).where((p,i) => i < 5);
+
+            //arraylist arraylist = new arraylist() { 1, 2, 3, "ahmed", "ali", 1.5, 2.3, 1.2f, 1.5m, productlist[0], productlist[1] };
+            //var result = arraylist.oftype<string>(); 
+            #endregion
+
+            foreach (var unit in result)
+            {
+                Console.WriteLine(unit);
+            }
+
         }
     }
 }
